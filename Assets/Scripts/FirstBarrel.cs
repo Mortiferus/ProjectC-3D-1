c3a043c6b813795441419c39ca84811e0a996761
Mortiferus <Mrnikinik@yahoo.com>
2023-08-29 17:32:46 +0300

    addScript

--- Assets/Scripts/FirstBarrel.cs
+++ Assets/Scripts/FirstBarrel.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FirstBarrel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ public GameObject[] Barrels = new GameObject[3];  // Переменная является массивом типа GameObject
+ public Transform[] transforms = new Transform[3];
+ public float speed = 1.0f, rotatespeed = 3f;
 
     // Update is called once per frame
-    void Update()
+    private void Start()    // Отвечает за запуск игры
     {
-        
+      
+    }
+    private void Update()    //Постоянное обновление после старта (40 фреймов в секунду)
+    {
+         for (int i=0; i < transforms.Length;i++)   // Вызов цикла
+         {
+            if (transforms[i] == null)
+               continue;
+            transforms[i].Translate(new Vector3(1,0,0) * speed * Time.deltaTime);    // Передвижение объекта
+            transforms[i].Rotate(new Vector3(1,0,0) * rotatespeed * Time.deltaTime); // Вращение объекта
+
+             float posX = transforms[i].position.x;                           // Создаём переменную и присваиваем ей позицию
+            if (posX > 5f && transforms[i].gameObject.name == "Barrel")   // если posX будет больше 10 и наименование объекта массива равно = 'бочка'
+               Destroy(transforms[i].gameObject);   // Уничтожить объект
+         }
     }
 }
