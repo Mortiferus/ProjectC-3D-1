a62263b3fa97d24568ade2360b83ddf7187ae814
Mortiferus <Mrnikinik@yahoo.com>
2023-08-30 19:08:48 +0300

    ar

--- Assets/Scripts/FirstBarrel.cs
+++ Assets/Scripts/FirstBarrel.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class FirstBarrel : MonoBehaviour
 {
- public GameObject[] Barrels = new GameObject[3];  // Переменная является массивом типа GameObject
- public Transform[] transforms = new Transform[3];
- public float speed = 1.0f, rotatespeed = 3f;
+ public GameObject[] Barrels = new GameObject[1];  // Переменная является массивом типа GameObject
+ public Transform[] transforms = new Transform[1];
+ public float speed = 1.0f, rotatespeed = 30f;
 
     // Update is called once per frame
     private void Start()    // Отвечает за запуск игры
     {
-      
+   
     }
     private void Update()    //Постоянное обновление после старта (40 фреймов в секунду)
     {
-         for (int i=0; i < transforms.Length;i++)   // Вызов цикла
-         {
-            if (transforms[i] == null)
-               continue;
-            transforms[i].Translate(new Vector3(1,0,0) * speed * Time.deltaTime);    // Передвижение объекта
-            transforms[i].Rotate(new Vector3(1,0,0) * rotatespeed * Time.deltaTime); // Вращение объекта
+   for (int i=0; i < transforms.Length;i++)
+       {
+         rotatespeed = 30f;
+         if (transforms[i] == null)
+               return;
+          transforms[i].Rotate(new Vector3(1,0,0) * rotatespeed * Time.deltaTime);
+          float posX = transforms[i].rotation.x;
+          if (posX > 20f && transforms[i].gameObject.name == "Barrel")
 
-             float posX = transforms[i].position.x;                           // Создаём переменную и присваиваем ей позицию
-            if (posX > 5f && transforms[i].gameObject.name == "Barrel")   // если posX будет больше 10 и наименование объекта массива равно = 'бочка'
-               Destroy(transforms[i].gameObject);   // Уничтожить объект
-         }
+        }
     }
+
+   public void BarrelDown()
+    {
+
+    }
+
+
 }
