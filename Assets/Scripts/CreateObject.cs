28e0b9a0b6281d1cb919f9bf2e95c3b1522bfbab
Mortiferus <Mrnikinik@yahoo.com>
2023-08-30 09:15:10 +0300

    newScriptCreateObjects

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+   public GameObject[] obj;
+
+     private void Start()
+     {
+        for (int i = 0; i < 5; i++)
+        {
+             Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(RandomNumber(),RandomNumber(),RandomNumber()), Quaternion.Euler(12f,-15f,40f));  
+        }
+       // GameObject newObject = Instantiate(obj, new Vector3(0,5,0), Quaternion.Euler(12f,-15f,40f)) as GameObject;
+       // newObject.GetComponent<Transform>().position= new Vector3(10,0,0);
+     }
+     private int RandomNumber()
+     {
+        return Random.Range(0, 10);
+     }
+ 
+}
--- Assets/Scripts/FirstBarrel.cs
+++ Assets/Scripts/FirstBarrel.cs
@@ -6,7 +6,7 @@ public class FirstBarrel : MonoBehaviour
 {
  public GameObject[] Barrels = new GameObject[3];  // Переменная является массивом типа GameObject
  public Transform[] transforms = new Transform[3];
- public float speed = 1.0f, rotatespeed = 3f;
+ public float speed = 1.0f, rotatespeed = 2f;
 
     // Update is called once per frame
     private void Start()    // Отвечает за запуск игры
@@ -17,13 +17,14 @@ public class FirstBarrel : MonoBehaviour
     {
          for (int i=0; i < transforms.Length;i++)   // Вызов цикла
          {
+
             if (transforms[i] == null)
                continue;
             transforms[i].Translate(new Vector3(1,0,0) * speed * Time.deltaTime);    // Передвижение объекта
-            transforms[i].Rotate(new Vector3(1,0,0) * rotatespeed * Time.deltaTime); // Вращение объекта
+            transforms[i].Rotate(new Vector3(90,0,0) * rotatespeed * Time.deltaTime); // Вращение объекта
 
              float posX = transforms[i].position.x;                           // Создаём переменную и присваиваем ей позицию
-            if (posX > 5f && transforms[i].gameObject.name == "Barrel")   // если posX будет больше 10 и наименование объекта массива равно = 'бочка'
+            if (posX > 7f && transforms[i].gameObject.name == "Barrel")   // если posX будет больше 10 и наименование объекта массива равно = 'бочка'
                Destroy(transforms[i].gameObject);   // Уничтожить объект
          }
     }
